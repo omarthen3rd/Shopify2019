14167a87950c32cd6884ea6e4713c17c103a178f
omarthen3rd <omarehsaan2@gmail.com>
2019-01-18 18:11:36 -0500

    Initial Commit

---
+++ MerchCollectionsTests/MerchCollectionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MerchCollectionsTests.swift
+//  MerchCollectionsTests
+//
+//  Created by Omar Abbasi on 2019-01-18.
+//  Copyright © 2019 Omar Abbasi. All rights reserved.
+//
+
+import XCTest
+@testable import MerchCollections
+
+class MerchCollectionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
